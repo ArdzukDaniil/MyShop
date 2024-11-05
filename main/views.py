82aa5ab0dd344104750a2537ac368221e1c9526a
Daniil Ardzuk <ardzyukdaniil@gmail.com>
2024-11-05 20:23:29 +0300

    Added app goods

--- main/views.py
+++ main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
 
 def index(request):
+    categories = Categories.objects.all()
     context = {
         'title': 'Home - Главаная ',
         'content': 'магазин мебели HOME',
+        'categories': categories,
     }
     return render(request, 'main/index.html', context)
 
