e544e55bd53f729e75279c6e5467c3c3d3aee01d
Daniil Ardzuk <ardzyukdaniil@gmail.com>
2025-04-03 19:59:59 +0300

    Login form and controller are ready

---
+++ users/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    # username = forms.CharField(
+    #     widget=forms.TextInput(attrs={'autofocus': True,
+    #                                   'class': 'form-control',
+    #                                   'placeholder': 'Введите имя пользователя'}))
+    # password = forms.CharField(
+    #     widget=forms.PasswordInput(attrs={'autocompile': "current-password",
+    #                                       'class': 'form-control',
+    #                                       'placeholder': 'Введите ваш пароль'})
+    #
+    # )
+
+
