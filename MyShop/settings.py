3de5328727027196cdb353c93e0b0add91ae17df
Daniil Ardzuk <ardzyukdaniil@gmail.com>
2025-04-14 12:44:59 +0300

    All cart templates were included

--- MyShop/settings.py
+++ MyShop/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'main',
     'goods',
     'users',
+    'carts',
+
 
 ]
 
---
+++ carts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def cart_add(request, product_if):
+    ...
+
+
+def cart_change(request, product_if):
+    ...
+
+
+def cart_remove(request, product_if):
+    ...
